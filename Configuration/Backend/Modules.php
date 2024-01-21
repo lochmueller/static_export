dd3d3d6b89473f31daa19c104f4a6e13d05b4637
Tim Lochmüller <tim@fruit-lab.de>
2024-01-21 23:01:27 +0100

    Backend Icons v12

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -10,6 +10,7 @@ return [
         'path' => '/module/file/static-export',
         'labels' => 'LLL:EXT:static_export/Resources/Private/Language/locallang_mod.xlf',
         'extensionName' => 'StaticExport',
+        'iconIdentifier' => 'static-export',
         'inheritNavigationComponentFromMainModule' => false,
         'controllerActions' => [
             Backend12Controller::class => [
---
+++ Configuration/Icons.php
@@ -0,0 +1,9 @@
+<?php
+
+
+return [
+    'static-export' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:static_export/Resources/Public/Icons/Extension.svg',
+    ],
+];
