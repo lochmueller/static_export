6125e1115e0471f8d9c0dc4869383c6b24526fb9
Tim Lochmüller <tim@fruit-lab.de>
2024-02-05 21:16:36 +0100

    Do not include namespaces

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,10 @@
 <?php
 
-use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-
-$configuration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('static_export');
+$configuration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('static_export');
 
 $curlResolve = isset($configuration['curlResolve']) ? $configuration['curlResolve'] : '';
 
-$configurations = GeneralUtility::trimExplode(',', (string)$curlResolve, true);
+$configurations = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', (string)$curlResolve, true);
 
 if (!empty($configurations)) {
     $GLOBALS['TYPO3_CONF_VARS']['HTTP']['curl'][\CURLOPT_RESOLVE] = $configurations;
